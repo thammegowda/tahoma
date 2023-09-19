e1dd85d0060c18324e6d978f5e6bb171677e6f02
Thamme Gowda <thammegowda@microsoft.com>
2023-09-19 14:16:50 +0000

    remove signal handler; now handled by backward-cpp

--- src/common/commons.hpp
+++ src/common/commons.hpp
@@ -1,14 +1,14 @@
-#define BACKWARD_HAS_UNWIND 1
+#pragma once
+//#define BACKWARD_HAS_UNWIND 1
 
 #include <iostream>
 #include <filesystem>
 #include <signal.h>
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/bundled/format.h>
-//#include <stacktrace>
-#include <backward.hpp>
 
 namespace fs = std::filesystem;
+namespace LOG = spdlog;
 
 /// @brief formatter for filesystem:path to work with spdlog
 // https://stackoverflow.com/a/69496952/1506477
@@ -25,35 +25,9 @@ struct fmt::formatter<fs::path> {
 };
 ///
 
-void signal_handler(int sig) {
-
-    spdlog::error("signal {}", sig);
-    //std::cerr << std::stacktrace::current();
-
-    using namespace backward;
-    StackTrace st;
-    st.load_here(32);
-    st.skip_n_firsts(3);
-
-    Printer p;
-    p.object = true;
-    p.color_mode = ColorMode::always;
-    p.address = true;
-    p.snippet = true;
-    p.trace_context_size = 3;
-    p.print(st, stderr);
-
-    // exit with signal number
-    exit(sig);
-}
-
-
-
 int global_setup() {
-    spdlog::info("Global setup started");
-    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [t%t] [%^%l%$] %v");
-    spdlog::set_level(spdlog::level::info);
-    signal(SIGSEGV, signal_handler);
-    signal(SIGABRT, signal_handler);
+    LOG::info("Global setup started");
+    LOG::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [t%t] [%^%l%$] %v");
+    LOG::set_level(LOG::level::info);
     return 0;
 }
