5bb2118d45f91d3beca272e6109cee4a3a57e6f0
TG Gowda <thammegowda@microsoft.com>
2023-10-29 19:52:20 -0700

    add own implementation of MultiheadAttention

--- src/nmt/transformer.hpp
+++ src/nmt/transformer.hpp
@@ -1,3 +1,5 @@
+#pragma once
+#include <tuple>
 #include <iostream>
 #include <tuple>
 #include <assert.h>
@@ -54,8 +56,73 @@ namespace rtg::nmt::transformer {
     };
     TORCH_MODULE(PositionEmbedding);
 
+
+    struct MultiheadAttentionImpl: public nn::Module {
+        nn::Linear q_proj;
+        nn::Linear k_proj;
+        nn::Linear v_proj;
+        nn::Linear out_proj;
+        nn::Dropout dropout;
+        int model_dim;
+        int nhead;
+
+        MultiheadAttentionImpl(int model_dim, int nhead, double dropout = 0.1) :
+            q_proj{ (assert(model_dim > 0), assert(nhead > 0), assert(model_dim % nhead == 0),
+                register_module("q_proj", nn::Linear(nn::LinearOptions(model_dim, model_dim)))) },
+            k_proj{ register_module("k_proj", nn::Linear(nn::LinearOptions(model_dim, model_dim))) },
+            v_proj{ register_module("v_proj", nn::Linear(nn::LinearOptions(model_dim, model_dim))) },
+            out_proj{ register_module("out_proj", nn::Linear(nn::LinearOptions(model_dim, model_dim))) },
+            dropout{ register_module("dropout", nn::Dropout(nn::DropoutOptions(dropout))) },
+            model_dim{ model_dim },
+            nhead{ nhead }
+        {}
+
+        auto forward(torch::Tensor& query, torch::Tensor& key, torch::Tensor& value, torch::Tensor& key_padding_mask) 
+            -> std::tuple<torch::Tensor, torch::Tensor> {
+            // query:   [batch_size, tgt_len, model_dim]
+            // key, val: [batch_size, src_len, model_dim]
+            // key_padding_mask: [batch_size, 1, T, src_len]
+            // return: [batch_size, tgt_len, model_dim], [batch_size, tgt_len, src_len]
+
+            auto tgt_len = query.size(1);
+            auto src_len = key.size(1);
+            auto batch_size = query.size(0);
+            assert(batch_size == key.size(0));
+
+            assert(query.size(2) == model_dim);
+            assert(key.size(2) == model_dim);
+            assert(value.size(2) == model_dim);
+
+            auto q = q_proj(query).view({ batch_size, tgt_len, nhead, model_dim / nhead }).transpose(1, 2); // [batch_size, nhead, tgt_len, model_dim / nhead]
+            auto k = k_proj(key).view({ batch_size, src_len, nhead, model_dim / nhead }).transpose(1, 2); // [batch_size, nhead, src_len, model_dim / nhead]
+            auto v = v_proj(value).view({ batch_size, src_len, nhead, model_dim / nhead }).transpose(1, 2); // [batch_size, nhead, src_len, model_dim / nhead]
+
+            auto attn_weights = torch::matmul(q, k.transpose(-2, -1)); // [batch_size, nhead, tgt_len, src_len]
+            attn_weights = attn_weights / std::sqrt(model_dim / nhead);
+
+            if (key_padding_mask.defined()) {
+                // insert head dim
+                //key_padding_mask = key_padding_mask.unsqueeze(1); // [batch_size, 1, 1, src_len]
+                assert(key_padding_mask.sizes().size() == 4); // must be a 4D tensor
+                float low_val = -1e9;   // TODO: check for float16 and bf16
+                attn_weights = attn_weights.masked_fill(key_padding_mask, low_val);
+            }
+            attn_weights = F::softmax(attn_weights, -1); // [batch_size, nhead, tgt_len, src_len]
+            attn_weights = dropout(attn_weights);
+            std::cout << "attn_weights" << attn_weights.sizes() << "\t v" << v.sizes() << std::endl;
+            auto attn_output = torch::matmul(attn_weights, v) // [batch_size, nhead, tgt_len, model_dim / nhead]
+                                .transpose(1, 2)              // [batch_size, tgt_len, nhead, model_dim / nhead]
+                                .contiguous().view({ batch_size, tgt_len, model_dim }); // [batch_size, tgt_len, model_dim]
+
+            attn_output = out_proj(attn_output); // [batch_size, tgt_len, model_dim]
+            return std::make_tuple(attn_output, attn_weights);
+        }
+    };
+    TORCH_MODULE(MultiheadAttention);
+    
+
     struct EncoderLayerImpl : public nn::Module {
-        nn::MultiheadAttention self_attn;
+        MultiheadAttention self_attn;
         nn::Linear fc1;
         nn::Linear fc2;
         nn::LayerNorm norm1;
@@ -64,10 +131,9 @@ namespace rtg::nmt::transformer {
         nn::Dropout dropout2;
 
         EncoderLayerImpl(int model_dim, int nhead, double dropout = 0.1) :
-           
             self_attn{
                 (assert(model_dim > 0), assert(nhead > 0), assert(model_dim % nhead == 0),
-                register_module("self_attn", nn::MultiheadAttention(nn::MultiheadAttentionOptions(model_dim, nhead).dropout(dropout))))
+                register_module("self_attn", MultiheadAttention(model_dim, nhead, dropout)))
                 },
             fc1{ register_module("fc1", nn::Linear(nn::LinearOptions(model_dim, model_dim * 4))) },
             fc2{ register_module("fc2", nn::Linear(nn::LinearOptions(model_dim * 4, model_dim))) },
@@ -132,7 +198,7 @@ namespace rtg::nmt::transformer {
             x = position_embedding(x); // [batch_size, src_len, model_dim]
             x = dropout(x);
             x = norm1(x);
-            
+
             for (int i = 0; i < num_layers; ++i) {
                 auto layer = layers->at<EncoderLayerImpl>(i);
                 x = layer.forward(x, src_mask);
@@ -145,8 +211,8 @@ namespace rtg::nmt::transformer {
 
     struct DecoderLayerImpl : public nn::Module {
 
-        nn::MultiheadAttention self_attn;
-        nn::MultiheadAttention src_attn;
+        MultiheadAttention self_attn;
+        MultiheadAttention src_attn;
         nn::Linear fc1;
         nn::Linear fc2;
 
@@ -158,19 +224,13 @@ namespace rtg::nmt::transformer {
         nn::Dropout dropout2;
         nn::Dropout dropout3;
 
-
         DecoderLayerImpl(int model_dim, int nhead, double dropout = 0.1) :
             self_attn {( 
                 assert(model_dim > 0), 
                 assert(nhead > 0),
                 assert(model_dim % nhead == 0), 
-                register_module("self_attn",
-                    nn::MultiheadAttention(nn::MultiheadAttentionOptions(model_dim, nhead).dropout(dropout)))
-            )},
-            src_attn {
-                register_module("src_attn", 
-                    nn::MultiheadAttention(nn::MultiheadAttentionOptions(model_dim, nhead).dropout(dropout))) 
-            },
+                register_module("self_attn", MultiheadAttention(model_dim, nhead, dropout)) )},
+            src_attn { register_module("src_attn", MultiheadAttention(model_dim, nhead, dropout)) },
             fc1 { register_module("fc1", nn::Linear(nn::LinearOptions(model_dim, model_dim * 4))) },
             fc2 { register_module("fc2", nn::Linear(nn::LinearOptions(model_dim * 4, model_dim))) },
             norm1 { register_module("norm1", nn::LayerNorm(nn::LayerNormOptions({ model_dim }))) },
@@ -207,28 +267,36 @@ namespace rtg::nmt::transformer {
     TORCH_MODULE(DecoderLayer);
 
     struct DecoderImpl: public nn::Module {
+        int num_layers;
+        int model_dim;
+        int nhead;
+
         PositionEmbedding position_embedding;
         nn::LayerNorm norm1;
         nn::Dropout dropout;
-        int num_layers;
         nn::ModuleList layers;
+        nn::Linear out_proj;
 
         DecoderImpl(int vocab_size, int model_dim, int nhead, int num_layers, double dropout = 0.1) :
-            position_embedding{( assert(vocab_size > 0), assert(model_dim > 0),
+            model_dim {( assert(model_dim > 0),  model_dim )},
+            num_layers {( assert(num_layers > 0), num_layers )},
+            nhead {( assert(nhead >0), assert(model_dim % nhead == 0),  nhead )},
+            position_embedding{( 
+                assert(vocab_size > 0), assert(model_dim > 0),
                 register_module("position_embedding", PositionEmbedding(vocab_size, model_dim, dropout)) 
                 )},
             norm1{ register_module("norm1", nn::LayerNorm(nn::LayerNormOptions({ model_dim }))) },
             dropout{ register_module("dropout", nn::Dropout(nn::DropoutOptions(dropout))) },
             layers{ register_module("layers", nn::ModuleList()) },
-            num_layers{ num_layers }
+            out_proj{ register_module("out_proj", nn::Linear(nn::LinearOptions(model_dim, vocab_size))) }
         {
-            assert(num_layers > 0);
             for (int i = 0; i < num_layers; ++i) {
                 layers->push_back(DecoderLayer(model_dim, nhead, dropout));
             }
         }
 
-        auto forward(torch::Tensor& tgt, torch::Tensor& memory, torch::Tensor& tgt_mask, torch::Tensor& memory_mask) -> torch::Tensor {
+        auto forward(torch::Tensor& tgt, torch::Tensor& memory, torch::Tensor& tgt_mask,
+            torch::Tensor& memory_mask) -> torch::Tensor {
             // tgt: [batch_size, tgt_len]
             // memory: [batch_size, src_len, model_dim]
             // tgt_mask: [batch_size, 1, tgt_len]
@@ -243,6 +311,7 @@ namespace rtg::nmt::transformer {
             for (int i = 0; i < num_layers; ++i) {
                 x = layers->at<DecoderLayerImpl>(i).forward(x, memory, tgt_mask, memory_mask);
             }
+            x = out_proj(x); // [batch_size, tgt_len, vocab_size]
             return x;
         }
     };
@@ -250,28 +319,35 @@ namespace rtg::nmt::transformer {
 
 
     struct TransformerNMTImpl : public nn::Module {
+        int src_vocab_size;
+        int tgt_vocab_size;
+        int model_dim;
+
         Encoder encoder;
         Decoder decoder;
 
         TransformerNMTImpl(const rtg::config::Config& config):
+            src_vocab_size { config["model"]["src_vocab_size"].as<int>() },
+            tgt_vocab_size { config["model"]["tgt_vocab_size"].as<int>() },
+            model_dim { config["model"]["model_dim"].as<int>() },
+
             encoder {
                 register_module("encoder", Encoder(
-                config["model"]["src_vocab_size"].as<int>(),
-                config["model"]["model_dim"].as<int>(),
+                src_vocab_size,
+                model_dim,
                 config["model"]["attn_head"].as<int>(),
                 config["model"]["encoder_layers"].as<int>(),
                 config["model"]["dropout"].as<double>()))
             },
             decoder {
                 register_module("decoder",  Decoder(
-                config["model"]["tgt_vocab_size"].as<int>(),
-                config["model"]["model_dim"].as<int>(),
+                tgt_vocab_size,
+                model_dim,
                 config["model"]["attn_head"].as<int>(),
                 config["model"]["decoder_layers"].as<int>(),
                 config["model"]["dropout"].as<double>()))
             }
-        {
-        }
+        {}
 
         auto forward(torch::Tensor& src, torch::Tensor& tgt, torch::Tensor& src_mask, torch::Tensor& tgt_mask) -> torch::Tensor {
             // src: [batch_size, src_len]
