97065c67982be95f1c735e0f3fe4ab3022dccaae
TG Gowda <thammegowda@microsoft.com>
2023-11-06 00:32:41 -0800

    build for cpu, cuda, rocm options

--- libs/setup.sh
+++ libs/setup.sh
@@ -1,31 +1,53 @@
 #!/usr/bin/env bash
 set -eu
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $MYDIR
 
-# PyTorch version
+# versions
+LIBTORCH_VERSION=2.1.0
+CUDA_VERSION=cu121
+ROCM_VERSION=rocm5.6
 
 libtorch_base="https://download.pytorch.org/libtorch"
-libtorch_version="libtorch-cxx11-abi-shared-with-deps-2.0.1"
+libtorch_version="libtorch-cxx11-abi-shared-with-deps-${LIBTORCH_VERSION}"
 
 cpu_url="${libtorch_base}/cpu/$libtorch_version%2Bcpu.zip"
-cuda_url="${libtorch_base}/cu118/$libtorch_version%2Bcu118.zip"
-rocm_url="${libtorch_base}/rocm5.4.2/$libtorch_version%2Brocm5.4.2.zip"
+cuda_url="${libtorch_base}/$CUDA_VERSION/$libtorch_version%2B${CUDA_VERSION}.zip"
+rocm_url="${libtorch_base}/$ROCM_VERSION/$libtorch_version%2B${ROCM_VERSION}.zip"
 
-libtorch_url=${cpu_url}
-# Download libtorch
-# testing cpu version for now
-if [[ -f libtorch/lib/libtorch.so ]]; then
-    echo "libtorch already exists"
-else
+download() {
+    local name="$1"
+    local url="$2"
+    local flag=$name/_SETUP_OK
+
+    if [[ -f $flag ]]; then
+        echo "$name already exists. Skipping. 'rm $flag' to force redownload."
+        return 0
+    fi
+    if [[ -f $name.zip && -f $name.zip._OK ]]; then
+        echo "$name already exists"
+    else
+        rm -f $name.zip $name.zip._OK
+        wget -O $name.zip "${url}" && touch $name.zip._OK
+    fi
+
+    rm -rf $name  # remove incomplete downloads
+    unzip $name.zip -d $name \
+        && mv $name/libtorch/* $name/ \
+        && rm -rf $name/libtorch \
+        && touch $flag
+}
+
+main() {
+    cd $MYDIR
     for tool in wget unzip; do
         if ! command -v $tool &> /dev/null; then
             echo "$tool could not be found. Please install and rerun."
-            exit
+            exit 2
         fi
     done
+    download libtorch-cpu "$cpu_url"
+    download libtorch-$CUDA_VERSION "$cuda_url"
+    #download libtorch-$ROCM_VERSION "$rocm_url"
+}
 
-    wget -O libtorch.cpu.zip $libtorch_url
-    unzip libtorch.cpu.zip
-    rm libtorch.cpu.zip
-fi
+main "$@"
