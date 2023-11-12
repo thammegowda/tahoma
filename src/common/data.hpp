8eec2d7ff131916916e4347d6a6c34c8adf87506
Thamme Gowda <thammegowda@microsoft.com>
2023-11-12 23:02:31 +0000

    move data loader to data.hpp

--- src/common/data.hpp
+++ src/common/data.hpp
@@ -3,10 +3,15 @@
 #include <iostream>
 #include <coroutine>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <memory>
+
 #include <__generator.hpp>  //reference implementation of generator
 #include <torch/torch.h>
 #include <sentencepiece_processor.h>
 #include <rtg.hpp>
+#include "config.hpp"
 #include "utils.hpp"
 
 
@@ -20,7 +25,7 @@ using namespace torch::indexing;
 using namespace rtg;
 
 namespace rtg::data {
-    
+
     struct Example {
 
         int64_t id;
@@ -28,13 +33,13 @@ namespace rtg::data {
         vector<vector<int32_t>> field_ids;
 
         Example(int64_t id, vector<string> fields, vector<vector<int32_t>> field_ids)
-        : id(id), fields(fields), field_ids(field_ids) {}
+            : id(id), fields(fields), field_ids(field_ids) {}
 
         ~Example() {}
-        
+
         //copy ctr
         Example(const Example& other)
-        : id(other.id), fields(other.fields), field_ids(other.field_ids) {}
+            : id(other.id), fields(other.fields), field_ids(other.field_ids) {}
 
         //copy assignment
         Example& operator=(const Example& other) {
@@ -47,19 +52,19 @@ namespace rtg::data {
         }
 
         // move ctr
-        Example(Example&& other) noexcept:
-         id(other.id), fields(other.fields), field_ids(other.field_ids) {
+        Example(Example&& other) noexcept :
+            id(other.id), fields(other.fields), field_ids(other.field_ids) {
         }
 
         friend std::ostream& operator<<(std::ostream& os, const Example& example);
-       
+
     };
 
     // operator<<
     std::ostream& operator<<(std::ostream& os, const Example& ex) {
-        os << "Example(" << ex.id << "; fields(" << 
-        ex.fields.size() << "): " << ex.fields 
-        << "; ids: (" << ex.field_ids.size() << "))";
+        os << "Example(" << ex.id << "; fields(" <<
+            ex.fields.size() << "): " << ex.fields
+            << "; ids: (" << ex.field_ids.size() << "))";
         return os;
     }
 
@@ -69,14 +74,14 @@ namespace rtg::data {
         vector<Example> examples;
 
         Batch(vector<torch::Tensor> fields, vector<Example> examples)
-        : fields(fields), examples(examples) {}
-    
-        static Batch from_buffer(vector<Example> buffer){
+            : fields(fields), examples(examples) {}
+
+        static Batch from_buffer(vector<Example> buffer) {
             /**
              * Convert a buffer of Examples to a Batch
             */
             int32_t batch_size = buffer.size();
-            assert (batch_size > 0);
+            assert(batch_size > 0);
             int32_t num_fields = buffer[0].field_ids.size();
 
             vector<int32_t> max_lens(num_fields, 0);
@@ -89,22 +94,127 @@ namespace rtg::data {
             vector<torch::Tensor> fields(num_fields);
             int64_t pad_id = 0;  // pad token id
             for (size_t i = 0; i < num_fields; ++i) {
-                fields[i] = torch::full({batch_size, max_lens[i]}, pad_id, torch::kLong);
+                fields[i] = torch::full({ batch_size, max_lens[i] }, pad_id, torch::kLong);
                 for (int32_t j = 0; j < buffer.size(); ++j) {
-                    auto ids = torch::tensor( buffer[j].field_ids[i], torch::kLong);
-                    fields[i].index_put_({j, Slice(0,  buffer[j].field_ids[i].size())}, ids);
+                    auto ids = torch::tensor(buffer[j].field_ids[i], torch::kLong);
+                    fields[i].index_put_({ j, Slice(0,  buffer[j].field_ids[i].size()) }, ids);
                 }
             }
             return Batch(fields, buffer);
         }
 
         auto to(torch::Device& device) -> Batch& {
-            for(auto idx = 0; idx < fields.size(); ++idx) {
+            for (auto idx = 0; idx < fields.size(); ++idx) {
                 fields[idx] = fields[idx].to(device);
             }
             return *this;
         }
-        
+
         ~Batch() {}
     };
+
+    auto load_vocabs(const config::Config& config) -> vector<std::shared_ptr<sp::SentencePieceProcessor>> {
+        auto vocab_paths = config["schema"]["vocabs"].as<std::vector<std::string>>();
+        assert(!vocab_paths.empty()); // expected atleast one vocabulary
+        // SentencePieceProcessor is not copyable and movable, so we use pointers
+        vector<std::shared_ptr<sp::SentencePieceProcessor>> spps;
+        for (auto vocab_path : vocab_paths) {
+            spdlog::debug("loading vocab {}", vocab_path);
+            auto spp = std::make_shared<sp::SentencePieceProcessor>();
+            if (!fs::exists(vocab_path)) {
+                spdlog::error("Vocab file {} not found", vocab_path);
+                throw runtime_error("Vocab file " + vocab_path + " not found");
+            }
+            if (!spp->Load(vocab_path).ok()) {
+                throw runtime_error("Unable to load vocab from " + vocab_path);
+            }
+            spps.push_back(spp);
+        }
+        return spps;
+    }
+
+    struct DataLoader {
+        /**
+         * A data loader that reads data from a list of files
+         * and returns batches of data
+        */
+        vector<std::shared_ptr<sp::SentencePieceProcessor>> vocabs;
+        config::Config config;
+
+        DataLoader(config::Config config, std::vector<std::shared_ptr<sp::SentencePieceProcessor>> vocabs)
+            : vocabs(vocabs), config(config)
+        {}
+
+        DataLoader(config::Config config)
+            : DataLoader(config, load_vocabs(config))
+        {}
+
+        ~DataLoader() {}
+
+        auto get_train_data() -> generator<data::Batch> {
+            auto data_paths = config["trainer"]["data"].as<vector<string>>();
+            auto batch_size = config["trainer"]["batch_size"].as<int>();
+            return get_data(data_paths, batch_size);
+        }
+
+        auto get_data(vector<string> data_paths, int64_t batch_size) -> generator<data::Batch> {
+            LOG::info("Loading data from {}", fmt::join(data_paths, ","));
+            assert(batch_size > 0);
+            const int32_t num_fields = data_paths.size();
+            vector<ifstream> files(num_fields);
+            for (size_t i = 0; i < num_fields; ++i) {
+                files[i].open(data_paths[i]);
+                if (!files[i]) {
+                    throw runtime_error("Failed to open file " + data_paths[i]);
+                }
+            }
+
+            int64_t rec_num = 0;
+            vector<data::Example> buffer;
+            vector<string> fields;
+            vector<vector<int32_t>> field_ids;
+            while (true) {
+                fields = vector<string>(num_fields);
+                field_ids = vector<vector<int32_t>>(num_fields);
+                bool has_ended = false;  // in any file
+                for (size_t i = 0; i < num_fields; i++) {
+                    if (!getline(files[i], fields[i]) || fields[i].empty()) {
+                        has_ended = true;
+                        spdlog::warn("file {} has no more data or there are empty rows. Stopping", data_paths[i]);
+                        break;
+                    }
+                }
+                if (has_ended) { break; }
+
+                bool skip = false;
+                for (size_t i = 0; i < num_fields; ++i) {
+                    auto ids = vocabs[i]->EncodeAsIds(fields[i]);
+                    skip = skip || ids.empty();
+                    field_ids[i] = ids;
+                }
+                if (skip) {
+                    spdlog::warn("Skipping empty record {}", rec_num);
+                    continue;
+                }
+
+                auto ex = data::Example(rec_num, fields, field_ids);
+                buffer.push_back(ex);
+                rec_num++;
+                if (buffer.size() >= batch_size) {
+                    co_yield data::Batch::from_buffer(buffer);
+                    buffer = vector<data::Example>();
+                }
+            }
+
+            if (!buffer.empty()) {
+                co_yield data::Batch::from_buffer(buffer);
+            }
+            // I wish there was a finally{} block to guarantee file closure :(
+            for (auto& file : files) {
+                file.close();
+            }
+        }
+
+    };
+
 }
--- src/nmt/trainer.hpp
+++ src/nmt/trainer.hpp
@@ -26,90 +26,68 @@ using namespace torch::indexing;
 using namespace rtg;
 
 
-//torch::Device device = torch::cuda::is_available() ? torch::kCUDA : torch::kCPU;
-
 namespace rtg::train {
 
-    enum Mode{
+    enum Mode {
         TRAINING,
         INFERENCE,
     };
 
+    template <typename M>
+    auto init_model(config::Config& config, torch::Device& device) -> M {
+        auto model_type = config["model"]["name"].as<string>();
+        if (model_type == "transformer") {
+            YAML::Node model_args = config["model"]["args"];
+            auto model = nmt::transformer::TransformerNMT(model_args);
+            //model->to(device);
+            return model;
+        }
+        else {
+            throw runtime_error("Unknown model type " + model_type);
+        }
+    }
 
-    auto load_vocabs(const config::Config& config) -> vector<std::shared_ptr<sp::SentencePieceProcessor>> {
-            auto vocab_paths = config["schema"]["vocabs"].as<std::vector<std::string>>();
-            assert(!vocab_paths.empty()); // expected atleast one vocabulary
-            // SentencePieceProcessor is not copyable and movable, so we use pointers
-            vector<std::shared_ptr<sp::SentencePieceProcessor>> spps;
-            for (auto vocab_path : vocab_paths) {
-                spdlog::debug("loading vocab {}", vocab_path);
-                auto spp = std::make_shared<sp::SentencePieceProcessor>();
-                if (!fs::exists(vocab_path)) {
-                    spdlog::error("Vocab file {} not found", vocab_path);
-                    throw runtime_error("Vocab file " + vocab_path + " not found");
-                }
-                if (!spp->Load(vocab_path).ok()) {
-                    throw runtime_error("Unable to load vocab from " + vocab_path);
-                }
-                spps.push_back(spp);
+    template <typename C>
+    auto init_criterion(const config::Config& config) -> C {
+        return nn::CrossEntropyLoss(nn::CrossEntropyLossOptions().reduction(torch::kNone));
+    }
+
+    template <typename M>
+    auto init_optimizer(const config::Config& config, M model)
+        -> std::shared_ptr<optim::Optimizer> {
+        auto optim_config = config["optimizer"];
+        auto optim_name = optim_config["name"].as<string>();
+        return std::make_shared<optim::Adam>(model->parameters(), optim::AdamOptions(0.0001));
+    }
+
+    auto init_scheduler(const config::Config& config, optim::Optimizer& optimizer)
+        -> std::shared_ptr<optim::LRScheduler> {
+        return std::make_shared<optim::StepLR>(optimizer, 1.0, 0.95);
+    }
+
+    auto init_config(fs::path work_dir, fs::path config_file) -> config::Config {
+        /*
+        * 1. If config_file is not provided, look for config.yaml in work_dir
+        * 2. If config_file is provided, copy it to work_dir and use it
+        * 3. If config_file is not provided and config.yaml is not found in work_dir, raise error
+        */
+        auto work_config = work_dir / "config.yaml";
+        if (!config_file.empty()) { // given non empty config_file
+            if (!fs::is_regular_file(config_file)) {
+                throw runtime_error(fmt::format("Config file {} not found", config_file.string()));
             }
-            return spps;
-        }
-
-        template <typename M>
-        auto init_model(config::Config& config, torch::Device& device) -> M {
-            auto model_type = config["model"]["name"].as<string>();
-            if (model_type == "transformer") {
-                YAML::Node model_args = config["model"]["args"];
-                auto model = nmt::transformer::TransformerNMT(model_args);
-                //model->to(device);
-                return model;
-            } else {
-                throw runtime_error("Unknown model type " + model_type);
+            if (!fs::exists(work_dir)) {
+                spdlog::info("mkdir {}", work_dir);
+                fs::create_directories(work_dir);
             }
+            spdlog::info("Copy {} ➡️ {}", config_file, work_config);
+            fs::copy(config_file, work_config, fs::copy_options::overwrite_existing);
         }
-
-        template <typename C>
-        auto init_criterion(const config::Config& config) -> C {
-            return nn::CrossEntropyLoss(nn::CrossEntropyLossOptions().reduction(torch::kNone));
+        if (!fs::exists(work_config)) {
+            throw runtime_error(fmt::format("Config file {} not found", work_config.string()));
         }
-
-        template <typename M>
-        auto init_optimizer(const config::Config& config, M model)
-             -> std::shared_ptr<optim::Optimizer> {
-            auto optim_config = config["optimizer"];
-            auto optim_name = optim_config["name"].as<string>();
-            return std::make_shared<optim::Adam>(model->parameters(), optim::AdamOptions(0.0001));
-        }
-
-        auto init_scheduler(const config::Config& config, optim::Optimizer& optimizer)
-             -> std::shared_ptr<optim::LRScheduler> {
-            return std::make_shared<optim::StepLR>(optimizer, 1.0, 0.95);
-        }
-
-        auto init_config(fs::path work_dir, fs::path config_file) -> config::Config {
-            /*
-            * 1. If config_file is not provided, look for config.yaml in work_dir
-            * 2. If config_file is provided, copy it to work_dir and use it
-            * 3. If config_file is not provided and config.yaml is not found in work_dir, raise error
-            */
-            auto work_config = work_dir / "config.yaml";
-            if (!config_file.empty()) { // given non empty config_file
-                if (!fs::is_regular_file(config_file)) {
-                    throw runtime_error(fmt::format("Config file {} not found", config_file.string()));
-                }    
-                if (!fs::exists(work_dir)){
-                    spdlog::info("mkdir {}", work_dir);
-                    fs::create_directories(work_dir);
-                }
-                spdlog::info("Copy {} ➡️ {}", config_file, work_config);
-                fs::copy(config_file, work_config, fs::copy_options::overwrite_existing);
-            }
-            if (!fs::exists(work_config)) {
-                throw runtime_error(fmt::format("Config file {} not found", work_config.string()));
-            }
-            return config::Config(config_file);
-        }
+        return config::Config(config_file);
+    }
 
     template <typename M, typename C>
     class Trainer {
@@ -120,96 +98,36 @@ namespace rtg::train {
         C criterion;
         std::shared_ptr<optim::Optimizer> optimizer;
         std::shared_ptr<optim::LRScheduler> scheduler;
-        vector<std::shared_ptr<sp::SentencePieceProcessor>> vocabs;
-        torch::Device device { torch::cuda::is_available() ? "cuda" :"cpu" };
+        rtg::data::DataLoader data_loader;
+        torch::Device device{ torch::cuda::is_available() ? "cuda" : "cpu" };
         int32_t chunk_size;
     public:
-        
-        Trainer(fs::path work_dir, config::Config conf):
-            work_dir {work_dir},
-            config {conf},
-            model {init_model<M>(config, device)},
-            criterion {init_criterion<C>(config)},
-            optimizer {init_optimizer(config, model)},
-            scheduler {init_scheduler(config, *optimizer)},
-            vocabs {load_vocabs(config)},
-            chunk_size {config["trainer"]["chunk_size"].as<int>(-1)}
+
+        Trainer(fs::path work_dir, config::Config conf)
+            : work_dir{ work_dir },
+            config{ conf },
+            model{ init_model<M>(config, device) },
+            criterion{ init_criterion<C>(config) },
+            optimizer{ init_optimizer(config, model) },
+            scheduler{ init_scheduler(config, *optimizer) },
+            data_loader{ rtg::data::DataLoader(config) },
+            chunk_size{ config["trainer"]["chunk_size"].as<int>(-1) }
         {
             spdlog::info("Trainer initialized; work_dir={} device = {}", work_dir, device == torch::kCUDA ? "cuda" : "cpu");
         }
 
         Trainer(fs::path work_dir, fs::path config_file)
-        : Trainer(work_dir, init_config(work_dir, config_file))
+            : Trainer(work_dir, init_config(work_dir, config_file))
         {}
 
         ~Trainer() {}
 
-        auto get_train_data() -> generator<data::Batch> {
-            
-            auto batch_size = config["trainer"]["batch_size"].as<int>();
-            auto data_paths = config["trainer"]["data"].as<vector<string>>();
-            LOG::info("Loading data from {}", fmt::join(data_paths, ","));
-            assert (batch_size > 0);
-            const int32_t num_fields = data_paths.size();
-            vector<ifstream> files(num_fields);
-            for (size_t i = 0; i < num_fields; ++i) {
-                files[i].open(data_paths[i]);
-                if (!files[i]) {
-                    throw runtime_error("Failed to open file " + data_paths[i]);
-                }
-            }
 
-            int64_t rec_num = 0;
-            vector<data::Example> buffer;
-            vector<string> fields;
-            vector<vector<int32_t>> field_ids;
-            while (true) {
-                fields = vector<string>(num_fields);
-                field_ids = vector<vector<int32_t>>(num_fields);
-                bool has_ended = false;  // in any file
-                for (size_t i = 0; i < num_fields; i++) {                     
-                    if (!getline(files[i], fields[i]) || fields[i].empty()) {
-                        has_ended = true;
-                        spdlog::warn("file {} has no more data or there are empty rows. Stopping", data_paths[i]);
-                        break;
-                    }
-                }
-                if (has_ended) { break; }
-
-                bool skip = false;
-                for (size_t i = 0; i < num_fields; ++i) {
-                    auto ids = vocabs[i]->EncodeAsIds(fields[i]);
-                    skip = skip || ids.empty();
-                    field_ids[i] = ids;
-                }
-                if (skip) {
-                    spdlog::warn("Skipping empty record {}", rec_num);
-                    continue;
-                 }
-
-                auto ex = data::Example(rec_num, fields, field_ids);
-                buffer.push_back(ex);
-                rec_num++;
-                if (buffer.size() >= batch_size) {
-                    co_yield data::Batch::from_buffer(buffer);
-                    buffer = vector<data::Example>();
-                }
-            }
-
-            if (!buffer.empty()) {
-                co_yield data::Batch::from_buffer(buffer);
-            }
-            // I wish there was a finally{} block to guarantee file closure :(
-            for (auto& file : files) {
-                file.close();
-            }
-        }
-
-        auto subsequent_mask(int64_t seq_len, torch::Device device=torch::kCPU) -> Tensor{
+        auto subsequent_mask(int64_t seq_len, torch::Device device = torch::kCPU) -> Tensor {
             // batch: [batch_size, seq_len]
             // pad_idx: padding token id; usually 0; ignore if -1
             // returns: [batch_size, seq_len, seq_len]
-            auto mask = torch::ones({seq_len, seq_len}, torch::dtype(torch::kInt8).device(device)); // all cells have 1
+            auto mask = torch::ones({ seq_len, seq_len }, torch::dtype(torch::kInt8).device(device)); // all cells have 1
             mask = torch::triu(mask, /*diagonal=*/1);            // upper triangle and diagonal are 1, lower diagonal are 0
             return mask;
         }
@@ -232,9 +150,9 @@ namespace rtg::train {
             }
             nn::AnyModule projector(model->lm_head);
             for (int32_t epoch = 0; epoch < num_epochs; epoch++) {
-                auto train_data = get_train_data();
+                auto train_data = data_loader.get_train_data();
                 for (auto batch : train_data) {
-                    if (fp16_enabled){  //__enter__()
+                    if (fp16_enabled) {  //__enter__()
                         at::autocast::set_enabled(true);
                     }
                     batch = batch.to(device);
@@ -249,7 +167,7 @@ namespace rtg::train {
                     //auto loss = simple_loss_computer(output, tgt_ids, projector, pad_id);
                     auto loss = loss_computer(output, tgt_ids, projector, pad_id, normalizer, Mode::TRAINING);
 
-                    if (fp16_enabled){  // __exit__()
+                    if (fp16_enabled) {  // __exit__()
                         at::autocast::clear_cache();
                         at::autocast::set_enabled(false);
                     }
@@ -262,29 +180,30 @@ namespace rtg::train {
                     tot_tokens += normalizer;
                     if (step_num % 25 == 0) {
                         spdlog::info("Step: {}; loss: {:.5f}; sents: {}; toks: {}",
-                                    step_num, loss.item().toFloat(), tot_sents, tot_tokens);
-                        }
+                            step_num, loss.item().toFloat(), tot_sents, tot_tokens);
+                    }
                     step_num++;
                 }
             }
         }
 
         auto loss_computer(Tensor features, Tensor labels, nn::AnyModule projector,
-         int64_t pad_id, int64_t normalizer=-1, Mode mode=Mode::TRAINING){
+            int64_t pad_id, int64_t normalizer = -1, Mode mode = Mode::TRAINING) {
 
-            if (chunk_size > 0){
+            if (chunk_size > 0) {
 
                 return chunked_loss_computer(features, labels, projector, pad_id, chunk_size, normalizer, mode);
-            } else {
+            }
+            else {
                 return simple_loss_computer(features, labels, projector, pad_id, normalizer, mode);
             }
-         }
+        }
 
         auto simple_loss_computer(Tensor features, Tensor labels, nn::AnyModule projector,
-         int64_t pad_id, int64_t normalizer=-1, Mode mode=Mode::TRAINING) -> Tensor{
+            int64_t pad_id, int64_t normalizer = -1, Mode mode = Mode::TRAINING) -> Tensor {
             auto output = projector.forward(features);
-            auto output_flat = output.view({output.size(0) * output.size(1), -1}); // [batch_size * seq_len, vocab_size]
-            auto labels_flat = labels.view({-1}); // [batch_size * seq_len]
+            auto output_flat = output.view({ output.size(0) * output.size(1), -1 }); // [batch_size * seq_len, vocab_size]
+            auto labels_flat = labels.view({ -1 }); // [batch_size * seq_len]
             Tensor loss = criterion(output_flat, labels_flat);  // [batch_size * seq_len]
             //exclude padding tokens from loss calculation
             loss.masked_fill_(labels_flat == pad_id, 0.0);
@@ -299,7 +218,7 @@ namespace rtg::train {
         }
 
         auto chunked_loss_computer(Tensor features, const Tensor labels, nn::AnyModule projector,
-            const int64_t pad_id, const size_t chunk_size, const int64_t normalizer=-1, const Mode mode=Mode::TRAINING) -> Tensor{
+            const int64_t pad_id, const size_t chunk_size, const int64_t normalizer = -1, const Mode mode = Mode::TRAINING) -> Tensor {
             /**
              * Compute loss in chunks to avoid OOM
              * features: [batch_size, seq_len, hidden_size]
@@ -323,8 +242,8 @@ namespace rtg::train {
             for (auto chunk_idx = 0z; chunk_idx < total_chunks; chunk_idx++) {
                 auto start = chunk_idx * chunk_size;
                 auto end = min((chunk_idx + 1) * chunk_size, seq_len);
-                auto chunk_features = features_isolated.index({Slice(), Slice(start, end), Slice()}).contiguous();
-                auto chunk_labels = labels.index({Slice(), Slice(start, end)}).contiguous();
+                auto chunk_features = features_isolated.index({ Slice(), Slice(start, end), Slice() }).contiguous();
+                auto chunk_labels = labels.index({ Slice(), Slice(start, end) }).contiguous();
                 auto chunk_loss = simple_loss_computer(chunk_features, chunk_labels, projector, pad_id, normalizer, mode);
                 total_loss += chunk_loss.item().toFloat();
             }
@@ -333,7 +252,7 @@ namespace rtg::train {
                 features.backward(features_isolated.grad().data());
             }
             return total_loss;
-         }
+        }
 
     };
 
@@ -362,11 +281,11 @@ int main(int argc, char* argv[]) {
     if (args.get<bool>("verbose")) {
         spdlog::set_level(spdlog::level::debug);
     }
-    auto work_dir = fs::path {args.get<std::string>("work_dir")};
+    auto work_dir = fs::path{ args.get<std::string>("work_dir") };
     auto config_file_arg = args.get<std::string>("config");
     fs::path config_file;
     if (!config_file_arg.empty()) {
-        config_file = fs::path {config_file_arg};
+        config_file = fs::path{ config_file_arg };
     }
     auto trainer = train::Trainer<nmt::transformer::TransformerNMT, nn::CrossEntropyLoss>(work_dir, config_file);
     trainer.train();
